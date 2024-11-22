bb622ccc0358dc3ee718f44f7e76bd5bb23c299c
Stefan Boonstra <stefanboonstra@pvh.com>
2024-11-22 14:50:30 +0100

    feat: add next-auth

---
+++ auth/auth.test.ts
@@ -0,0 +1,9 @@
+import { auth } from "./auth";
+
+describe("auth", () => {
+  it("should do something", () => {
+    const result = auth();
+
+    expect(result).toBe({}); // Not reached, error thrown at line 5
+  });
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,22 +4,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [react()],
   test: {
-    clearMocks: true,
-    coverage: {
-      exclude: ["**/*.stories.ts?(x)", "**/*.mock?(Data).ts?(x)"],
-      include: [
-        "app",
-        "auth",
-        "components",
-        "conf",
-        "containers",
-        "helpers",
-        "i18n",
-        "layouts",
-        "services",
-      ],
-      reporter: ["html", "text", "text-summary", "cobertura", "lcov"],
-    },
     environment: "jsdom",
     globals: true,
     include: ["**/*.test.ts?(x)"],
